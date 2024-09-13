8b61d1ad4a3dc3c1e7df923a451034dc630602de
Gerdo Wijaya <gerdo.tewel@gmail.com>
2024-09-13 13:45:52 +0800

    test update deploy

--- src/index.js
+++ src/index.js
@@ -5,28 +5,28 @@ const port = 3000;
 
 app.get("/", (req, res) => {
   res.status(200).json({
-    message: "GET request",
+    message: "GET request from iFabula test app",
   });
 });
 
 app.post("/", (req, res) => {
   res.status(201).json({
-    message: "POST request",
+    message: "POST request from iFabula test app",
   });
 });
 
 app.put("/", (req, res) => {
   res.status(201).json({
-    message: "PUT request",
+    message: "PUT request from iFabula test app",
   });
 });
 
 app.delete("/", (req, res) => {
   res.status(202).json({
-    message: "DELETE request",
+    message: "DELETE request from iFabula test app",
   });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`iFabula test app started on port ${port}`);
 });
